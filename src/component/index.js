3b993d006c88c8bd83ef9c812da675d6327ac0cc
CINDY <git@github.com:CINDYMUTHON/challenge2.git>
2023-01-11 08:45:10 +0300

    first commit

---
+++ src/component/index.js
@@ -0,0 +1,16 @@
+function toDoList(){
+    return(
+        <h1>To-do-list</h1>
+
+    );
+}
+function toDoItem(){
+    return(
+    <ol>
+        <li>wake-up</li>
+        <li>bath</li>
+        <li>eat</li>
+        <li>do labs</li>
+</ol>
+)
+    }
---
+++ src/views/index.js
@@ -0,0 +1,7 @@
+function Home(){
+    return(
+        <div> <h3 className></h3>
+            
+        </div>
+    );
+    }
